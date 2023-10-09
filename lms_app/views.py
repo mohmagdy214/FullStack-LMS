60aa020d2f51f0b97a1d06f8f783760f6f0ff988
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-09 21:23:33 +0300

    add forms

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from .forms import BookForm
 
 # Create your views here.
 
@@ -8,11 +9,13 @@ def index(request):
     context = {
         'categories': Category.objects.all(),
         'books': Book.objects.all(),
+        'form': BookForm()
     }
     return render(request, 'pages/index.html', context)
 
 def books(request):
     context = {
         'categories': Category.objects.all(),
+        'books': Book.objects.all(), 
     }
     return render(request, 'pages/books.html', context)
