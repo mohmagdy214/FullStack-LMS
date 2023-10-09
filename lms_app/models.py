0d90d3603e6e142db1b02cea179cc470f8c2a621
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-09 19:04:22 +0300

    add 60% of LMS fullstack project

---
+++ lms_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Book(models.Model):
+    
+    status_book = [
+        ('Available','Available'),
+        ('Rented','Rented'),
+        ('Sold','Sold'),
+    ]
+
+
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    photo_book = models.ImageField(upload_to='photos', null=True, blank=True)
+    photo_author = models.ImageField(upload_to='photos', null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    rental_price_day = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    rental_period = models.IntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=50, choices=status_book, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
+
+
+    def __str__(self) -> str:
+        return self.title
---
+++ lms_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'categories': Category.objects.all(),
+        'books': Book.objects.all(),
+    }
+    return render(request, 'pages/index.html', context)
+
+def books(request):
+    context = {
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'pages/books.html', context)
