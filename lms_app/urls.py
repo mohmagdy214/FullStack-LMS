c4f6fbaba0d2a177e04f09b1feb0541be75fb87e
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-10 21:22:44 +0300

    update the books from the site

--- lms_app/urls.py
+++ lms_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books', views.books, name='books'),
-
+    path('<int:id>', views.update, name='update'),
 ]
--- lms_app/views.py
+++ lms_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import BookForm, CategoryForm
 
@@ -30,3 +30,19 @@ def books(request):
         'books': Book.objects.all(), 
     }
     return render(request, 'pages/books.html', context)
+
+def update(request, id):
+    book_id = Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save = BookForm(request.POST, request.FILES, instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect('/')
+        
+    else:
+        book_save = BookForm(instance=book_id)
+    
+    context = {
+        'form':book_save,
+    }
+    return render(request, 'pages/update.html', context)
