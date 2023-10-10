ccc5993e6ab3e627078c918eb95293a91a9445b1
Mahmoud Magdy <115575025+mohmagdy214@users.noreply.github.com>
2023-10-10 20:56:21 +0300

    make categories form to add new categories from the site

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from .forms import BookForm
+from .forms import BookForm, CategoryForm
 
 # Create your views here.
 
@@ -11,10 +11,16 @@ def index(request):
         if add_book.is_valid():
             add_book.save()
 
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+
     context = {
         'categories': Category.objects.all(),
         'books': Book.objects.all(),
-        'form': BookForm()
+        'form': BookForm(),
+        'formcat': CategoryForm(),
     }
     return render(request, 'pages/index.html', context)
 
